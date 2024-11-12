d53582b46ccb9e260d4f3a0a46a44654a8f4a7fb
Alex Cesar da Silva <thealexcesar@gmail.com>
2024-11-12 17:39:16 -0300

    Feat/home/hero (#6)
    * ✨ ui: mobile first + directive
    
    * ✨ internationalization
    
    * eslint
    
    * ✨ home/hero
    
    * 🛠  es-lint
    
    * 🛠  CI
    
    * ✨ hero component + whastapp btn + environments

--- src/app/components/home/hero/hero.component.ts
+++ src/app/components/home/hero/hero.component.ts
@@ -1,14 +1,24 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
+import {TranslatePipe} from '@ngx-translate/core';
+import {MatIcon} from '@angular/material/icon';
+import { faWhatsapp } from '@fortawesome/free-brands-svg-icons';
+import {FaIconComponent} from '@fortawesome/angular-fontawesome';
+import {environment} from '@environments/environment';
 
 @Component({
   selector: 'shd-hero',
   standalone: true,
-  imports: [],
+  imports: [
+    TranslatePipe,
+    MatIcon,
+    FaIconComponent,
+  ],
   templateUrl: './hero.component.html',
-  styles: `
-
-  `
+  styles: ``
 })
 export class HeroComponent {
-
+  
+  phoneNumber = environment.phoneNumber;
+  protected readonly  faWhatsapp = faWhatsapp;
+  
 }

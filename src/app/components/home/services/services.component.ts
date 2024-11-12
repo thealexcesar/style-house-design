2615d10d0455f0e30e3ef724cf80b53a89fa679c
alex cesar <thealexcesar@gmail.com>
2024-11-12 19:31:03 -0300

    ✨ component/services

---
+++ src/app/components/home/services/services.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'shd-services',
+  standalone: true,
+  imports: [],
+  templateUrl: './services.component.html',
+  styles: ``
+})
+export class ServicesComponent {
+
+}

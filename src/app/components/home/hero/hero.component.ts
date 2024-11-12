1519b8dfb47525ad94abf3a38aefbbdb2ec69f33
alex cesar <thealexcesar@gmail.com>
2024-11-12 17:35:55 -0300

    ✨ hero component + whastapp btn + environments

--- src/app/components/home/hero/hero.component.ts
+++ src/app/components/home/hero/hero.component.ts
@@ -1,14 +1,27 @@
 import { Component } from '@angular/core';
+import {TranslatePipe} from '@ngx-translate/core';
+import {MatIcon} from '@angular/material/icon';
+import { faWhatsapp } from '@fortawesome/free-brands-svg-icons';
+import {FaIconComponent} from '@fortawesome/angular-fontawesome';
+import {environment} from '@environments/environment';
 
 @Component({
   selector: 'shd-hero',
   standalone: true,
-  imports: [],
+  imports: [
+    TranslatePipe,
+    MatIcon,
+    FaIconComponent,
+  ],
   templateUrl: './hero.component.html',
-  styles: `
-
-  `
+  styles: ``
 })
 export class HeroComponent {
 
+  phoneNumber = environment.phoneNumber;
+
+  protected readonly  faWhatsapp = faWhatsapp;
+  sendMessage() {
+    console.log('hello')
+  }
 }

80022a6da12925a5d2efd095b91e8969b1855948
alex cesar <thealexcesar@gmail.com>
2024-11-13 16:46:18 -0300

    ✨ reasons components

---
+++ src/app/components/home/reasons/reasons.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import {TranslatePipe} from '@ngx-translate/core';
+
+@Component({
+  selector: 'shd-reasons',
+  standalone: true,
+  imports: [
+    TranslatePipe
+  ],
+  templateUrl: './reasons.component.html',
+  styles: ``
+})
+export class ReasonsComponent {
+
+}
